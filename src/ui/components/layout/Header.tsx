98848620fa48c42997c5a38447a109c2df70fdf7
Gabriel Dante <gabriel.dante3@gmail.com>
2025-09-30 20:45:10 -0300

    Update logo

--- src/ui/components/layout/Header.tsx
+++ src/ui/components/layout/Header.tsx
@@ -1,9 +1,17 @@
+// import logo from "";
+import logo from "../../assets/shadow_proxy_text_color.svg";
+
 export default function Header() {
   return (
     <>
       <div className="c-logo col-start-1 row-start-1 min-h-[3rem] min-w-fit">
-        <header className="h-12 flex items-center px-4 text-center">
-          <h1 className=" w-full text-center text-green-500 text-xl">Shadow</h1>
+        <header className="h-12 flex items-center px-2 text-center">
+          <img
+            src={logo}
+            alt="Shadow Proxy Logo"
+            className="w-full h-full object-cover object-[50%_40%]"
+          />
+          {/* <h1 className=" w-full text-center text-green-500 text-xl">Shadow</h1> */}
         </header>
       </div>
       <div className='col-start-2 row-start-1 min-h-[3rem] min-w-0'>
