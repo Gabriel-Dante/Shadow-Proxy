b3b12e3d64052a25032086a21f20ec8a2500be22
F3540541 Gabriel Dante Martins Mendes <gabriel.dante@bb.com.br>
2025-10-16 13:36:22 -0300

    Update alias import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
@@ -13,6 +13,14 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
   build: {
     outDir: "dist-react"
   },
